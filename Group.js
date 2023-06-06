879e78258252e09af7d830894a7064f092ccc170
LA Abiera <130994662+Areiba07@users.noreply.github.com>
2023-06-06 20:20:27 +0800

    updated function

--- Group.js
+++ Group.js
@@ -1,7 +1,13 @@
-function checkAttendance {
-
+function attendance(status){
+    let response = "Absent"
+    if(("minahal" === status) || ("nasaktan" === status) || ("nagmahal" === status)){
+        response = "Present"
+    }
+    return response
 }
 
-console.log(checkAttendance)
+console.log(attendance("minahal"))
+console.log(attendance("nasaktan"))
+console.log(attendance("nagmahal"))
 
 // attendance function
